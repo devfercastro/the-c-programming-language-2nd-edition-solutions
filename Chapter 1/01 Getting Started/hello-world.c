6c80d17bbabcca13f1befb5c3929efea689d15ca
devfercastro <devfercastro@proton.me>
2026-03-01 19:33:21 -0300

    reestructuring

---
+++ Chapter 1/01 Getting Started/hello-world.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Include information about standard library
+
+// Define a function called main that received no argument values statements of
+// main are enclosed in braces main calls library function printf to print this
+// sequence of characters \n represents the newline character
+int main() { printf("Hello, world\n"); }
